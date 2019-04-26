d316ece7eedda9c484112f8cdb0739879572b409
Sangin-Khidirov <48213526+Sangin-Khidirov@users.noreply.github.com>
2019-04-26 23:21:25 +0500

    7

---
+++ HashTable/HashTable/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Hash
+{
+    public class Program
+    {
+        class Item
+        {
+            public object Value;
+            public readonly int Key;
+            public Item(int key, object value)
+            {
+                Value = value;
+                Key = key;
+            }
+        }
+
+        public class HashTable
+        {
+            private List<Item>[] list;
+
+            public HashTable(int size)
+            {
+                list = new List<Item>[size];
+            }
+
+            public void PutPair(object key, object value)
+            {
+                var keyHashCode = key.GetHashCode();
+                var id = Math.Abs(keyHashCode) % list.Length;
+                var item = new Item(keyHashCode, value);
+
+                if (list[id] == null)
+                {
+                    list[id] = new List<Item> { item };
+                }
+
+                else
+                {
+                    var val = list[id].FirstOrDefault(x => x.Key == keyHashCode);
+
+                    if (val != null)
+                        val.Value = value;
+                    else
+                        list[id].Add(item);
+                }
+            }
+
+            public object GetValueByKey(object key)
+            {
+                try
+                {
+                    var keyHashCode = key.GetHashCode();
+                    var id = Math.Abs(keyHashCode) % list.Length;
+                    var value = list[id];
+                    return value.FirstOrDefault(x => x.Key == keyHashCode).Value;
+                }
+
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ HashTable/HashTableTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HashTableTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HashTableTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6063709a-83d9-4d56-af11-c0175c76558d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
